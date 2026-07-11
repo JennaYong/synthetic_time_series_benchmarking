bb86c5fb0a546ab0c1b5a19743da72f5ab670062
JennaYong <>
2026-07-11 12:21:29 -0400

    Modify gpu config for sbatch

--- job.sh
+++ job.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 #SBATCH --account=def-chenh
-#SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_2g.20gb:1
+#SBATCH --gpus-per-node=nvidia_h100_80gb_hbm3_2g.20gb:2
 #SBATCH --cpus-per-task=8
 #SBATCH --mem=32G
-#SBATCH --time=08:00:00
+#SBATCH --time=24:00:00
 #SBATCH --job-name=sssd_benchmark
 #SBATCH --output=logs/job-%j.out
 #SBATCH --error=logs/job-%j.err
